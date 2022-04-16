ffb1f0c4705f1262d6a75b26b1bcc078deeb3dd1
Jia-Wei-Liao <sam23582211@gmail.com>
2022-04-16 14:59:06 +0800

    ver2

--- HW2/test.py
+++ HW2/test.py
@@ -1,66 +1,53 @@
-import os
-import argparse
-from tqdm import tqdm
-
+from os import path
+if path.exists("model.py"):
+	from model import *
+from torchvision import transforms, datasets
 import torch
-import torchvision
-from src.config import *
-from src.dataset import get_test_loader
-from src.trainer import compute_acc
-from src.utils import *
-from sklearn.metrics import classification_report
+import glob
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+fpth = glob.glob('*.txt')
+print(fpth)
+if not fpth:
+	mean = (0.5071, 0.4867, 0.4408) 
+	std = (0.2675, 0.2565, 0.2761)
+	
+else:
+	with open(fpth[0]) as f:
+		lines = f.readlines()
+		for i, line in enumerate(lines):
+			if i == 0:
+				mean = (float(line.split(' ')[0]), float(line.split(' ')[1]), float(line.split(' ')[2].replace('\n', '')))
+			else:
+				std = (float(line.split(' ')[0]), float(line.split(' ')[1]), float(line.split(' ')[2].replace('\n', '')))
 
 
-def test(args):
-    test_loader = get_test_loader(args)
-    model = get_model(args)
-    model.load(os.path.join('checkpoint', args.checkpoint, 'weight', args.weight))
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model.to(device)
+print(mean, std)
+test_transform = transforms.Compose(
+[transforms.ToTensor(),
+transforms.Normalize(mean, std)])
 
-    model.eval()
-    total_num, correct = 0, 0
-    test_bar = tqdm(test_loader, desc=f'Testing')
-    pred_list = []
-    label_list = []
+testset = datasets.CIFAR100(root='./data', train=False,
+download=True, transform=test_transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=32,
+shuffle=False, num_workers=2)
 
-    with torch.no_grad():
-        for batch_data in test_bar:
-            image, label = batch_data
-            image = image.to(device)
-            label = label.to(device)
-            pred = model(image)
-            acc = compute_acc(pred, label)
-            num = image.shape[0]
-            total_num += num
-            correct += acc * num
-            pred = pred.argmax(dim=1)
-            pred_list += list(pred.cpu().numpy())
-            label_list += list(label.cpu().numpy())
+mpth = glob.glob('*.pth')
+model = torch.load(mpth[0])
+model.to(device)
+model.eval() # add this command
 
-            del image, label, pred
-            mean_acc = correct / total_num
-            test_bar.set_postfix({
-                'acc': f"{mean_acc:.5f}"
-            })
-        test_bar.close()
-
-    print(classification_report(label_list, pred_list, target_names=[str(i) for i in range(219)]))
-
-    
-
-    return
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--checkpoint', type=str,
-                        default='04-11-20-01-09',
-                        help='weight path')
-    parser.add_argument('--weight', type=str,
-                        default='ep=0160-acc=0.8607.pth',
-                        help='weight path')
-    args = parser.parse_args()
-    config = load_json(os.path.join('checkpoint', args.checkpoint, 'config.json'))
-    args = argparse.Namespace(**vars(args), **config)
-    test(args)
+# fixed testing process
+correct = 0
+total = 0
+with torch.no_grad():
+	for data in testloader:
+		images, labels = data
+		images = images.to(device)
+		labels = labels.to(device)
+		outputs = model(images)
+		# the class with the highest energy is what we choose as prediction
+		_, predicted = torch.max(outputs.data, 1)
+		total += labels.size(0)
+		correct += (predicted == labels).sum().item()
+	print(f'Accuracy of the network on the 10000 test images: {100 * correct / total:.2f} %')
